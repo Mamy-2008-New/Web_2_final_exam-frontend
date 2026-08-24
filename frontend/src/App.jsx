ee6b132f32aefd2b44c04e94b1fc788946701d3c
Mamy <hei.mamy.10@gmail.com>
2026-08-24 11:42:37 +0300

    Modification + ajout de fichier

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,15 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LoginPage from './pages/auth/LoginPage';
-import AdminDashboard from './pages/admin/AdminDashboard';
 import StudentExams from './pages/student/StudentExams';
+import TakeExam from './pages/student/TakeExam';
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LoginPage />} />
-        <Route path="/admin/*" element={<AdminDashboard />} />
-        <Route path="/student/*" element={<StudentExams />} />
+        <Route path="/student" element={<StudentExams />} />
+        <Route path="/student/exam/:id" element={<TakeExam />} />
       </Routes>
     </BrowserRouter>
   );
